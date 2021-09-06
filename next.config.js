3ec0877f60f893dcf483b8dbabbf11900ec94a64
kazuemon <git@k6n.jp>
2021-09-06 14:36:07 +0900

    add: 短縮URLのフォールバック, 作成中のテキストを追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const redirects = async () => {
+  return [
+    {
+      source: '/tw',
+      destination: 'https://twitter.com/kazuemon_0602',
+      permanent: true
+    },
+    {
+      source: '/gh',
+      destination: 'https://github.com/kazuemon',
+      permanent: true
+    },
+    {
+      source: '/profcard',
+      destination: 'https://profcard.info/u/sPkkk7IKGJeTH9PlBNyj4kBITFf1',
+      permanent: true
+    },
+    {
+      source: '/yt',
+      destination: 'https://youtube.com/c/kazuemon',
+      permanent: true
+    },
+  ]
 }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  redirects
+}
+
+module.exports = nextConfig;
---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Poppins&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument;
